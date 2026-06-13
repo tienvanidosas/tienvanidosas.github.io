52c365f7bfa15a2f98c06d9582e150594949b511
Luis Domínguez López <33281350+domlopluis94@users.noreply.github.com>
2026-06-13 18:19:50 +0200

    error de subida

---
+++ docs/assets/fLayout.astro_astro_type_script_index_0_lang.Duly1zR7.js
@@ -0,0 +1,1 @@
+import e from"https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";new e(".mySwiper",{slidesPerView:3,spaceBetween:20,loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},breakpoints:{320:{slidesPerView:1},768:{slidesPerView:2},1024:{slidesPerView:3}}});
---
+++ docs/assets/fastro.CMew3C3n.js
@@ -0,0 +1,3 @@
+import 'kleur/colors';
+import './fastro/server.CG65eha9.js';
+import 'clsx';
